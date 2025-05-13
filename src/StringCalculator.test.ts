11d02d4eeb81bbef467164a94de91c81912e642c
nirali-3151 <145057715+nirali-3151@users.noreply.github.com>
2025-05-13 22:37:33 +0530

    write test case : should return 0 for empty string

---
+++ src/StringCalculator.test.ts
@@ -0,0 +1,12 @@
+import { add } from './StringCalculator';
+
+describe('String Calculator', () => {
+    describe('add', () => {
+
+        it('should return 0 for empty string', () => {
+            expect(add('')).toBe(0);
+        });
+
+        
+    });
+}); 
