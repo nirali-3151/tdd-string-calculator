20071af246df9f3e57fbde9b026a192abf67ff6c
nirali-3151 <145057715+nirali-3151@users.noreply.github.com>
2025-05-13 22:44:36 +0530

    pass test - should return the sum of two numbers separated by a comma

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,5 +2,8 @@ export const add = (numbers: string): number => {
     if (!numbers) {
         return 0;
     }
+    if (numbers.includes(',')) {
+        return numbers.split(',').reduce((sum, n) => sum + parseInt(n, 10), 0);
+    }
     return parseInt(numbers);
 }; 
